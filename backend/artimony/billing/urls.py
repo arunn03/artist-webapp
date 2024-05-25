754fc26b888caf91655be5a4c0d789a399a1fcd8
Arunkumar S <iamsarunn03@gmail.com>
2024-05-25 23:58:12 +0530

    Removed gitignore files from repo

--- backend/artimony/billing/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from .views import CreateSubscriptionView
-
-urlpatterns = [
-    path('create-subscription/', CreateSubscriptionView.as_view(), name='create-subscription'),
-]
